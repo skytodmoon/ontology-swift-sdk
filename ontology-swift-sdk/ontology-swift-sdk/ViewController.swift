af1c340d8dff5001e22aa4effefa8005b3dae172
孙琦 <398266898@qq.com>
2018-07-24 10:51:39 +0800

    修正到低版本

--- ontology-swift-sdk/ontology-swift-sdk/ViewController.swift
+++ ontology-swift-sdk/ontology-swift-sdk/ViewController.swift
@@ -2,8 +2,8 @@
 //  ViewController.swift
 //  ontology-swift-sdk
 //
-//  Created by 孙琦 on 2018/7/23.
-//  Copyright © 2018年 孙琦. All rights reserved.
+//  Created by SH-JRY-0073 on 2018/7/24.
+//  Copyright © 2018年 com.wm. All rights reserved.
 //
 
 import UIKit
