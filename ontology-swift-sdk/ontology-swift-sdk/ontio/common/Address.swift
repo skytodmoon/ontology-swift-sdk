f32f8db9fe6e60d49b8c7902a14f0957616745f0
孙琦 <398266898@qq.com>
2018-07-25 17:27:35 +0800

    Address-part

--- ontology-swift-sdk/ontology-swift-sdk/ontio/common/Address.swift
+++ ontology-swift-sdk/ontology-swift-sdk/ontio/common/Address.swift
@@ -10,5 +10,22 @@
  * it mostly used to defined contract address
  */
 public class Address {
-    
+   static let ZERO:Address? = nil
+   static let COIN_VERSION: UInt8 = 0x17
+    init(value: String) throws {
+        if value.count == 0 {
+            throw Exception.NullPointerException
+        }
+        var tempValue: String = value
+        if value .starts(with: "0x") {
+            tempValue = String(value.prefix(2))
+        }
+        if tempValue.count != 40 {
+            throw Exception.IllegalArgumentException
+        }
+        let v: [UInt8] = Array(tempValue.utf8)
+    }
+    init(value: [UInt8]) {
+        
+    }
 }
