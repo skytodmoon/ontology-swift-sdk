13b37b84dd70b1ba619cf7bc6912bacf767d5ddc
孙琦 <398266898@qq.com>
2018-08-01 17:28:20 +0800

    start to add encrypt parts.

--- ontology-swift-sdk/ontology-swift-sdk/ontio/account/Account.swift
+++ ontology-swift-sdk/ontology-swift-sdk/ontio/account/Account.swift
@@ -8,9 +8,32 @@
 
 public class Account {
     fileprivate var keyType: KeyType?
-    fileprivate var curveParams:[AnyObject]?
+    fileprivate var curveParams:[Any]?
     fileprivate var privateKeyStr:String?
     fileprivate var publicKeyStr:String?
     fileprivate var addressU160:Address?
     fileprivate var signatureScheme:SignatureScheme?
+    
+    // create an account with the specified key type
+    init(scheme: SignatureScheme) throws {
+    signatureScheme = scheme
+        if scheme == SignatureScheme.SHA256WITHECDSA {
+            keyType = KeyType.ECDSA
+            curveParams = [Curve.P256.getCurveStr()]
+        }else if scheme == SignatureScheme.SM3WITHSM2{
+            keyType = KeyType.SM2
+            curveParams = [Curve.SM2P256V1.getCurveStr()]
+        }
+        
+        switch scheme {
+        case SignatureScheme.SHA256WITHECDSA,SignatureScheme.SM3WITHSM2:
+            if !(curveParams![0] is String){
+                throw ErrorCode.InvalidParams
+            }
+            
+            
+        default:
+            throw ErrorCode.UnsupportedKeyType
+        }
+    }
 }
--- ontology-swift-sdk/ontology-swift-sdk/ontio/crypto/CurveModel.swift
+++ ontology-swift-sdk/ontology-swift-sdk/ontio/crypto/CurveModel.swift
@@ -6,11 +6,19 @@
 //  Copyright © 2018年 com.wm. All rights reserved.
 //
 
-public struct CurveModel1{
+public struct CurveModel{
     var v0: Int
     var v1: String
     init(v0:Int,v1:String) {
         self.v0 = v0
         self.v1 = v1
     }
+    public func getV0() -> Int{
+        return v0
+    }
+    public func getV1() -> String{
+        return v1
+    }
 }
+
+
