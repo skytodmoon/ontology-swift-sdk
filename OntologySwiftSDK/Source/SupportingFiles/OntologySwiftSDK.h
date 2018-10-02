51a54fd2d79ad8b9a2d47b4c8f5586ae38451ddc
孙琦 <398266898@qq.com>
2018-10-02 08:45:03 +0800

    1. rebuild as a SDK project 2. Add cryptoLib-EllipticCurveKit

---
+++ OntologySwiftSDK/Source/SupportingFiles/OntologySwiftSDK.h
@@ -0,0 +1,19 @@
+//
+//  OntologySwiftSDK.h
+//  OntologySwiftSDK
+//
+//  Created by 孙琦 on 2018/10/2.
+//  Copyright © 2018年 孙琦. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OntologySwiftSDK.
+FOUNDATION_EXPORT double OntologySwiftSDKVersionNumber;
+
+//! Project version string for OntologySwiftSDK.
+FOUNDATION_EXPORT const unsigned char OntologySwiftSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OntologySwiftSDK/PublicHeader.h>
+
+
